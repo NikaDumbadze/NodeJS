1e261155cffb5d812daa3aa3124b666db1c38ce9
Nika Dumbadze <ndumbadze@betlive.com>
2024-10-07 14:05:18 +0400

    adding middleware

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // allow the request to contimiu to next middleware in line
+})
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  //...
+})
+
 server.listen(3000);
