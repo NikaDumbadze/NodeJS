87069f2e5d374019993750d1faf6a9bd0acca5af
Nika Dumbadze <ndumbadze@betlive.com>
2024-10-08 11:04:39 +0400

    working with express

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const http = require('http');
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extanded: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // allow the request to contimiu to next middleware in line
-})
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
-  //...
-})
-
-server.listen(3000);
+app.listen(3000);
